20e9d158576dbf4614951e7cd2279af4b9262c63
lukeabatayo <luke.abatayo17@gmail.com>
2022-02-07 11:37:40 +0800

    Reset and Updated Header and Footer, Updated Main Section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
@@ -7,22 +6,16 @@ const Header = () => {
         <header>
             <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
                 <Container>
-                    <LinkContainer to='/'>
-                        <Navbar.Brand>ShopMnl</Navbar.Brand>
-                    </LinkContainer>
+                    <Navbar.Brand href='/'>ShopMnl</Navbar.Brand>
                     <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                    <Navbar.Collapse id='navbarScroll'>
+                    <Navbar.Collapse id='basic-navbar-nav'>
                         <Nav className='ml-auto'>
-                            <LinkContainer to='/cart'>
-                                <Nav.Link>
-                                    <i className='fas fa-shopping-cart'></i> Cart
-                                </Nav.Link>
-                            </LinkContainer>
-                            <LinkContainer to='/login'>
-                                <Nav.Link>
-                                    <i className='fas fa-user'></i> Sign In
-                                </Nav.Link>
-                            </LinkContainer>
+                            <Nav.Link href='/cart'>
+                                <i className='fas fa-shopping-cart'></i> Cart
+                            </Nav.Link>
+                            <Nav.Link href='/login'>
+                                <i className='fas fa-suser'></i> Sign In
+                            </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
